d0631a759d7696e53994d47198bd2c99e04ea4b6
mervealgi <72434705+mervealgi@users.noreply.github.com>
2021-04-29 03:01:35 +0300

    textarea

--- src/Calculator.java
+++ src/Calculator.java
@@ -4,22 +4,31 @@ import java.awt.event.*;
 
 public class Calculator implements ActionListener {
 
-    JFrame frame;
-    JTextField textFile;
-    JButton[] numButtons = new JButton[10];
+    JFrame pencere;                                                 //Uygulama dış çerçevesi
+    JTextField textAlani;                                           //Input girilecek alanlar
+    JButton[] numButtons = new JButton[10];                         //Tutulan buton sayıları
     JButton[] fonsButon = new JButton[8];
     JButton ekleButon, cikButon, bolButon, carpButon;
     JButton desimButon, silButon, temButon, esitButon;
     JPanel panel;
-    Font panelFontu = new Font("Calibri",Font.BOLD,12);
+    Font pencereFontu = new Font("Calibri",Font.BOLD,12);
+
+    double num1, num2, sonuc = 0;
+    char operatorler;
 
     Calculator(){
 
-        frame = new JFrame("Calculator");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(420,550);
-        frame.setLayout(null);
-        frame.setVisible(true);
+        pencere = new JFrame("Calculator");                     //Uygulama dış çerçevesi yaratıldı
+        pencere.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        pencere.setSize(420,550);
+        pencere.setLayout(null);
+
+        textAlani = new JTextField();                               //Input alanı yaratıldı
+        textAlani.setBounds(50,25,300,50);      //Input alanı ayarlandı
+        textAlani.setFont(pencereFontu);
+
+        pencere.add(textAlani);                                     //Yaratılan her modül ekleniyor
+        pencere.setVisible(true);                                   //Pencereyi görünür kıl
 
     }
 
