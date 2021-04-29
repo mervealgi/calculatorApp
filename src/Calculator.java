aef60d15935636785e99edd8735ec18debb7bdc3
mervealgi <72434705+mervealgi@users.noreply.github.com>
2021-04-29 03:32:34 +0300

    buttons

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,10 +6,10 @@ public class Calculator implements ActionListener {
 
     JFrame pencere;                                                 //Uygulama dış çerçevesi
     JTextField textAlani;                                           //Input girilecek alanlar
-    JButton[] numButtons = new JButton[10];                         //Tutulan buton sayıları
-    JButton[] fonsButon = new JButton[8];
+    JButton[] numButon = new JButton[10];                         //Tutulan buton sayıları
+    JButton[] fonksButon = new JButton[8];
     JButton ekleButon, cikButon, bolButon, carpButon;
-    JButton desimButon, silButon, temButon, esitButon;
+    JButton desimButon, silButon, temizButon, esitButon;
     JPanel panel;
     Font pencereFontu = new Font("Calibri",Font.BOLD,12);
 
@@ -26,6 +26,26 @@ public class Calculator implements ActionListener {
         textAlani = new JTextField();                               //Input alanı yaratıldı
         textAlani.setBounds(50,25,300,50);      //Input alanı ayarlandı
         textAlani.setFont(pencereFontu);
+
+        ekleButon = new JButton("+");
+        cikButon = new JButton("-");
+        carpButon = new JButton("*");
+        bolButon = new JButton("/");
+        esitButon = new JButton("=");
+        desimButon = new JButton(".");
+        silButon = new JButton("Sil");
+        temizButon = new JButton("Temizle");
+
+        fonksButon[0] = ekleButon;
+        fonksButon[1] = cikButon;
+        fonksButon[2] = carpButon;
+        fonksButon[3] = bolButon;
+        fonksButon[4] = esitButon;
+        fonksButon[5] = desimButon;
+        fonksButon[6] = silButon;
+        fonksButon[7] = temizButon;
+
+
 
         pencere.add(textAlani);                                     //Yaratılan her modül ekleniyor
         pencere.setVisible(true);                                   //Pencereyi görünür kıl
